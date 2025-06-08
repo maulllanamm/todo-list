3e71d353b51459cbcd42de3a1b5319244db9981d
Maulana Muhammad <maulllanamuhammad@gmail.com>
2025-06-08 16:44:11 +0700

    feat : types todo.ts

---
+++ src/types/todo.ts
@@ -0,0 +1,9 @@
+export type Priority = "low" | "medium" | "high";
+export type Filter = "all" | "active" | "completed";
+
+export interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+  priority: Priority;
+}
